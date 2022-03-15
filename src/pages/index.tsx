ffcca461ca864c662b76c469ec3144101dc26461
SOOCHUL SON <zerozerobase@gmail.com>
2022-03-15 14:09:34 +0900

    :sparkles: 스터디 카드 UI 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,58 @@
 import type { NextPage } from 'next';
+import styled from '@emotion/styled';
+
 import { TitleSearch } from '../components/TitleSearch';
+import { StudyCard } from '../components/StudyCard';
 
 const Home: NextPage = () => {
   return (
     <>
       <TitleSearch />
-    </>
+      <StudyList>
+        <StudyCard
+          category = '카테고리'
+          title = 'React 강의 듣기'
+          hashtag = '#리액트 #프론트엔드'
+          member = {1}
+          link = '#'
+        />
+        <StudyCard
+          category = '카테고리'
+          title = 'React 강의 듣기'
+          hashtag = '#리액트 #프론트엔드'
+          member = {1}
+          link = '#'
+        />
+        <StudyCard
+          category = '카테고리'
+          title = 'React 강의 듣기'
+          hashtag = '#리액트 #프론트엔드'
+          member = {1}
+          link = '#'
+        />
+        <StudyCard
+          category = '카테고리'
+          title = 'React 강의 듣기'
+          hashtag = '#리액트 #프론트엔드'
+          member = {1}
+          link = '#'
+        />
+        <StudyCard
+          category = '카테고리'
+          title = 'React 강의 듣기'
+          hashtag = '#리액트 #프론트엔드'
+          member = {1}
+          link = '#'
+        />
+      </StudyList>
+    </>  
   );
 };
+
+const StudyList = styled.section`
+display: grid;
+grid-template-columns: repeat(4, 1fr);
+gap: 20px;
+`;
 
 export default Home;
