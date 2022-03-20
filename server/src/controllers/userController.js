61d9d28854660a263f6524c5471b3685caf17911
aiden <zero2one23581@gmail.com>
2022-03-20 21:28:35 +0900

    before merge

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,9 +1,7 @@
 import User from "../model/User";
 
 //CREATE
-export const getJoin = (req, res) => {
-  return res.send("join");
-};
+
 export const postJoin = async (req, res) => {
   const { userId, password, name, email, location } = req.body;
   console.log(userId, password, name, email, location);
--- server/src/routers/rootRouter.js
+++ server/src/routers/rootRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
+import { postJoin, getLogin, postLogin } from "../controllers/userController";
 
 const rootRouter = express.Router();
 
-rootRouter.route("/join").get(getJoin).post(postJoin);
+rootRouter.route("/join").post(postJoin);
 rootRouter.route("/login").get(getLogin).post(postLogin);
 
 export default rootRouter;
