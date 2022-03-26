f3aa91997a4fa00d14be3422e9cb4ae1875df1d1
aiden <zero2one23581@gmail.com>
2022-03-26 18:47:14 +0900

    🔥 client내 서버제거

--- client/server/router/rootRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { postJoin, postLogin } from '../controllers/userController';
-
-const rootRouter = express.Router();
-
-rootRouter.route('/api/join').post(postJoin);
-rootRouter.route('/login').post(postLogin);
-
-export default rootRouter;
