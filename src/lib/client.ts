70731d663936054025632f8310bc7d729068722a
aiden <zero2one23581@gmail.com>
2022-03-28 18:26:51 +0900

    ✨ API (서버) 생성!

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-// export default new PrismaClient();
-const client = new PrismaClient();
-
-client.user.create({
-  data: {
-    email: 'kjw@naver.com',
-    name: 'Junwoo',
-  },
-});
+export default new PrismaClient();
---
+++ src/pages/api/client-test.tsx
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from '../../lib/client';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  await client.user.create({
+    data: {
+      name: 'junwoo',
+      email: 'test@moss.com',
+    },
+  });
+  res.json({
+    ok: true,
+    data: 'hello world',
+  });
+}
