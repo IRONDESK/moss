551276740f8262a560ca1c53140abe47fa2bdb5a
aiden <zero2one23581@gmail.com>
2022-03-30 02:15:57 +0900

    ⚡️ 회원가입으로 계정생성

--- src/pages/api/users/join.tsx
+++ src/pages/api/users/join.tsx
@@ -3,9 +3,31 @@ import client from 'src/libs/server/client';
 import withHandler from 'src/libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // return res.status(200).end();
-  return res.json({ test: '백엔드 연결 성공' });
+  const { userId, password, username, location, email } = req.body;
+  let user;
+  if (userId && password && username && location && email) {
+    user = await client.user.findFirst({
+      where: { userId, password, username, location, email },
+    });
+    //해당하는 유저가 있다면?
+    if (user) console.log(`유저를 찾았습니다!`);
+    //해당하는 유저가 db에 없다면? -> 유저를 생성한다.
+    if (!user) {
+      user = await client.user.create({
+        data: {
+          userId,
+          password,
+          username,
+          location,
+          email,
+        },
+      });
+      console.log(`해당하는 유저가 없습니다. 입력한 정보로 유저를 생성합니다.`);
+    }
+    console.log(user);
+  }
+
+  return res.status(200).end();
 }
 
-//!!중요!! NextJs에서 api route를 만들때는 항상 export default를 해줘야 함!
 export default withHandler('POST', handler);
