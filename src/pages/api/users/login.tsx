221c2b4871b2f8f7c07071f058e31ee414eb32db
aiden <zero2one23581@gmail.com>
2022-03-29 23:40:00 +0900

    ⚡️ 유저아이디 비번으로 계정생성

--- src/pages/api/users/login.tsx
+++ src/pages/api/users/login.tsx
@@ -5,6 +5,29 @@ import withHandler from 'src/libs/server/withHandler';
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone, userId, password } = req.body; //1. fe에서 받은 데이터
   let user;
+  if (userId && password) {
+    //2. 아이디를 입력했다면 ->  정보와 일치하는 유저를 찾는다
+    user = await client.user.findFirst({
+      where: { userId, password },
+    });
+    //2-1. 해당하는 유저가 있다면?
+    if (user) console.log(`유저를 찾았습니다!`);
+    //2-2. 해당하는 유저가 db에 없다면? -> 유저를 생성한다.
+    if (!user) {
+      user = await client.user.create({
+        data: {
+          username: 'Anonymous',
+          userId,
+          password,
+        },
+      });
+      console.log(
+        `해당하는 유저가 없습니다. 입력한 아이디와 비번으로 유저를 생성합니다.`,
+      );
+    }
+    console.log(user);
+  }
+
   if (email) {
     //2. 이메일을 입력했다면 ->  그 이메일과 일치하는 유저를 찾는다
     user = await client.user.findUnique({
