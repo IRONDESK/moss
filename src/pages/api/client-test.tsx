511dfdce145eda67a051b5d9348308b1bd815e32
aiden <zero2one23581@gmail.com>
2022-03-29 20:35:50 +0900

    ♻️ 경로 baseUrl

--- src/pages/api/client-test.tsx
+++ src/pages/api/client-test.tsx
@@ -1,18 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import client from '../../libs/server/client';
+import client from 'src/libs/server/client';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
-) {
-  await client.user.create({
-    data: {
-      name: 'junwoo',
-      email: 'test@moss.com',
-    },
-  });
-  res.json({
-    ok: true,
-    data: 'hello world',
-  });
-}
+) {}
--- src/pages/api/users/login.tsx
+++ src/pages/api/users/login.tsx
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import client from '../../../libs/server/client';
-import withHandler from '../../../libs/server/withHandler';
+import withHandler from 'src/libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
