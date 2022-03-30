5ae0de8b94b892684e3b9914bec22bfcaf45ef63
aiden <zero2one23581@gmail.com>
2022-03-30 22:50:48 +0900

    ♻️ 🎨  ✅ 토큰 로그인 리팩토링 최종

--- src/pages/api/users/login.tsx
+++ src/pages/api/users/login.tsx
@@ -5,43 +5,46 @@ import withHandler from 'src/libs/server/withHandler';
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   //1. FE에서 받은 유저 데이터
   const { email, phone, userId, password } = req.body;
-  const user = email ? { email } : { phone: +phone };
+
+  //유저 찾기용
+  const toFindUser =
+    userId && password
+      ? { userId_password: { userId, password } }
+      : email
+      ? { email }
+      : { phone: +phone };
+
+  //유저 생성용
+  const toCreateUser =
+    userId && password
+      ? { userId, password }
+      : email
+      ? { email }
+      : { phone: +phone };
 
   const payload = Math.floor(100000 + Math.random() * 900000) + ''; //6자리 랜덤숫자 string
-  //토큰 생성
+
+  //2. 토큰 생성 -> 유저가 존재? -> 유저를 연결 // 유저가 없으면? -> 유저를 생성하고 연결
   const token = await client.token.create({
     data: {
       payload,
       user: {
         connectOrCreate: {
           where: {
-            ...(userId && password
-              ? { userId_password: { userId, password } }
-              : {}),
+            ...toFindUser,
           },
           create: {
             username: 'Anonymous',
-            ...(userId && password ? { userId, password } : {}),
+            ...toCreateUser,
           },
         },
       },
     },
   });
-  //
+
+  //확인
   console.log('Token here!', token);
   return res.status(200).end();
 }
 
-//!!중요!! NextJs에서 api route를 만들때는 항상 export default를 해줘야 함!
 export default withHandler('POST', handler);
-//2-1. 아이디 비번으로 로그인시
-// if (userId && password) {
-//   const user = await client.user.findFirst({
-//     where: { userId, password },
-//   });
-//   if (user) console.log(`유저를 찾았습니다!`);
-//   if (!user) {
-//     console.log(`해당하는 유저가 없습니다.`);
-//   }
-//   console.log(user);
-// }
