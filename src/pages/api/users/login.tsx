9cde52d06603138c54205f537b24c3b96d2c91a7
aiden <zero2one23581@gmail.com>
2022-03-30 22:43:54 +0900

    ♻️  유저 아이디 비번 리팩토링

--- src/pages/api/users/login.tsx
+++ src/pages/api/users/login.tsx
@@ -7,39 +7,41 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone, userId, password } = req.body;
   const user = email ? { email } : { phone: +phone };
 
-  //2-1. 아이디 비번으로 로그인시
-  if (userId && password) {
-    const user = await client.user.findFirst({
-      where: { userId, password },
-    });
-    if (user) console.log(`유저를 찾았습니다!`);
-    if (!user) {
-      console.log(`해당하는 유저가 없습니다.`);
-    }
-    console.log(user);
-  } else {
-    const payload = Math.floor(100000 + Math.random() * 900000) + ''; //6자리 랜덤숫자 string
-    const token = await client.token.create({
-      data: {
-        payload,
-        //3-1. 토큰 생성 -> 유저와 연결 // 유저가 없다면? -> 유저를 생성 (토큰과 연결됨)
-        user: {
-          connectOrCreate: {
-            where: {
-              ...user,
-            },
-            create: {
-              username: 'Anonymous',
-              ...user,
-            },
+  const payload = Math.floor(100000 + Math.random() * 900000) + ''; //6자리 랜덤숫자 string
+  //토큰 생성
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...(userId && password
+              ? { userId_password: { userId, password } }
+              : {}),
+          },
+          create: {
+            username: 'Anonymous',
+            ...(userId && password ? { userId, password } : {}),
           },
         },
       },
-    });
-    console.log('토큰생성!', token);
-  }
+    },
+  });
+  //
+  console.log('Token here!', token);
   return res.status(200).end();
 }
 
 //!!중요!! NextJs에서 api route를 만들때는 항상 export default를 해줘야 함!
 export default withHandler('POST', handler);
+//2-1. 아이디 비번으로 로그인시
+// if (userId && password) {
+//   const user = await client.user.findFirst({
+//     where: { userId, password },
+//   });
+//   if (user) console.log(`유저를 찾았습니다!`);
+//   if (!user) {
+//     console.log(`해당하는 유저가 없습니다.`);
+//   }
+//   console.log(user);
+// }
