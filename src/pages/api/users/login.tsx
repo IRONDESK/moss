0b4ea1d1aa85c7f0b5d3ce21bc29b4969f9cce35
aiden <zero2one23581@gmail.com>
2022-03-31 12:25:36 +0900

    ✨ SMS 인증 set up

--- src/pages/api/users/login.tsx
+++ src/pages/api/users/login.tsx
@@ -1,8 +1,14 @@
+import twilio from 'twilio';
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from 'src/libs/server/client';
-import withHandler from 'src/libs/server/withHandler';
+import withHandler, { ResponseType } from 'src/libs/server/withHandler';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_ACCT, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) {
   //1. FE에서 받은 유저 데이터
   const { email, phone, userId, password } = req.body;
 
@@ -13,6 +19,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       : email
       ? { email }
       : { phone: +phone };
+
+  if (!toFindUser) return res.status(400).json({ ok: false }); // Bad Request
 
   //유저 생성용
   const toCreateUser =
@@ -41,10 +49,18 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
+  //SMS 전송
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!, //현재 tiwilio trial 계정임으로 실제 서비스를 구동할때 유저의 번호를 넣어주면 된다.
+      body: `당신 받을 토큰번호는 ${payload}`,
+    });
+    console.log(message);
+  }
 
   //확인
-  console.log('Token here!', token);
-  return res.status(200).end();
+  return res.json({ ok: true });
 }
 
 export default withHandler('POST', handler);
