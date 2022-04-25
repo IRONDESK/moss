32a4ae682152384b0f70375878fa177e627f42ea
SOOCHUL SON <zerozerobase@gmail.com>
2022-04-25 19:59:58 +0900

    :recycle: studyinfo schema에 category 추가

--- src/pages/api/study/getInfo.tsx
+++ src/pages/api/study/getInfo.tsx
@@ -4,15 +4,4 @@ export default function getInfo(id: string|string[]|undefined) {
     const { data } = useSWR(`/api/study/create?id=${id}`);
     console.log(data);
     return data;
-
-    // if (id) {
-    //     const res = fetch(`/api/study/create?id=${id}`, {
-    //         method: 'GET',
-    //     })
-    //     .then((response) => response.json().catch(() => {}))
-    //     .then((json) => json)
-    //     .catch((error) => console.log("error", error));
-    
-    //     return res;
-    // }
 };
